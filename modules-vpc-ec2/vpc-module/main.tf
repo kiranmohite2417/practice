4c22c5248d59e47967252d7cb5859f6fb3000abf
root <root@ip-172-31-47-192.ap-south-1.compute.internal>
2025-05-09 09:22:06 +0000

    upgrade corrected

--- modules-vpc-ec2/vpc-module/main.tf
+++ modules-vpc-ec2/vpc-module/main.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "public" {
   cidr_block              = element(var.public_subnets, count.index)
   availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
+
   tags = {
     Name = "${var.vpc_name}-public-${count.index}"
   }
